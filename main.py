0f39bc19d1e9bac7a2ea6e55e62336d6aa77d008
Rustem <115068281+Achimas@users.noreply.github.com>
2022-12-08 08:18:30 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+def search_link
+	site_url = "..."
+
+	r = request.get(site_url)
+
+	with open(r) as r:
+	soup = BeautifulSoup(r, 'html.parser')
+	
+
