51fc4da4fac409b0a86eb702f042daaccee1f4c1
Rustem <115068281+Achimas@users.noreply.github.com>
2022-12-08 12:17:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,21 @@ def open_excel(excel_name):
 	ws = wb.active
 	all_names = []
 	for row in ws.iter_rows(values_only=True):  
-		all_names.append(row['1'])  # to do
+		all_names.append(row['B'])  # to do
 	wb.save(f'_{excel_name}')
 	return all_names
 
-def search_object(all_names):
+def search_objects(all_names):
 	site_url = "..."  # to do
 	r = request.get(site_url)  # to do
 	with open(r) as r:
 		soup = BeautifulSoup(r, 'html.parser')  # to do
+	parents_to_xls = []
 	for name in all_names:
 		link = soup.find(name)  # to do
 		all_parents = get_parents(name, link)
+		parents_to_xls.append(all_parents)
+	return parents_to_xls
 
 def get_parents(name, link):
 	all_parents_list = []
@@ -38,9 +41,17 @@ def get_parents(name, link):
 		parent_name_now = adding_parent
 	return all_parents_list
 			
-def adding_parents_excel (all_parents_list, excel_name):
+def adding_parents_excel (parents_to_xls, excel_name):
 	wb = Workbook(excel_name)
 	ws = wb.active
-	ws.insert_cols(1) # вставляем колонку перед основным названием
-	...
+	ws.insert_cols(1) # вставляем 5 колонок перед основным названием
+	ws.insert_cols(1)
+	ws.insert_cols(1)
+	ws.insert_cols(1)
+	ws.insert_cols(1)
+	cells_symbol = ['B', 'C', 'D', 'E', 'F']
+	for i in range(len(ws.rows)-1):
+		for j in range(5):
+			cell = f'{cells_symbol}{i+2}'
+			ws[cell]=parents_to_xls[i][j]
 	wb.save(f'v2_{excel_name}')
