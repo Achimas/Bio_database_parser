4d98fa414d6b77ef628c9738bf9bff5a34202729
Rustem <115068281+Achimas@users.noreply.github.com>
2022-12-08 10:09:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,34 @@ def open_excel(excel_name):
 	ws = wb.active
 	all_names = []
 	for row in ws.iter_rows(values_only=True):  
-		??? all_names.append(row['1'])
+		all_names.append(row['1'])  # to do
 	new_filename = f'v2_{excel_name}'
 	wb.save(filename = new_filename)
 	return all_names
 
 def search_object(all_names):
-	site_url = "..."
-	r = request.get(site_url)
+	site_url = "..."  # to do
+	r = request.get(site_url)  # to do
 	with open(r) as r:
-		soup = BeautifulSoup(r, 'html.parser')
+		soup = BeautifulSoup(r, 'html.parser')  # to do
 	for name in all_names:
-		link = soup.find(name)
+		link = soup.find(name)  # to do
+		all_parents = get_parents(name, link)
 
-def get_parents(link):
-	parents_list = []
-	parent_now = ''
-	while parent_now == 'r = request.get(link)
-	
+def get_parents(name, link):
+	all_parents_list = {}
+	parent_name_now = {}
+	parent_name_now[name] = link
+	while 'pathways' not in parent_name_now:
+		if parent_name_now < 2:
+			r = request.get(link)  # to do
+			soup = BeautifulSoup(r, 'html.parser')  # to do
+			links_name = soup.find_all("a", class_='...') #class = '' как-то там называющийся класс-родитель
+			links = soup.find_all("a", class_='...') # .get (href) # class = '' как-то там называющийся класс-родитель
+			for link in links_name:
+				parent_name_now.append(link.text()) # to do
+			all_parents_list.append(', '.join(parent_name_now) if len(parent_name_now)>1 else parent_name_now)
+			
+		else:
+			
+			
